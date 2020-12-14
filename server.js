b457a35536f9ca854f6a041ae9c0612c0baec1a4
Elijam <elijam1608@gmail.com>
2020-12-14 20:10:04 +1100

    fixed err

--- server.js
+++ server.js
@@ -25,11 +25,11 @@ mongoose.connect(MONGODB_URL,{
     userFindAndModify:false
 })
 
- const htmlRouts = require("./routes/apiRoutes")(app);
- const apiRoutes = require("./routes/htmlRouts")(app);
+ require("./routes/apiRoutes")(app);
+ require("./routes/htmlRouts")(app);
 
-app.use(htmlRouts);
-app.use(apiRoutes);
+// app.use(htmlRouts);
+// app.use(apiRoutes);
 
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}!`);
